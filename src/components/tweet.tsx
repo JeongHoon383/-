4df83551aa60d9650fa2cdf8d16aeb781a2c3156
Jeong Hoon <gbg9609@naver.com>
2024-10-01 23:36:25 +0900

    Feat : 트윗 삭제

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,8 @@
 import { styled } from "styled-components";
 import { ITweet } from "./timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
   display: grid;
@@ -27,12 +30,46 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
-export default function Tweet({ username, photo, tweet }: ITweet) {
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const user = auth.currentUser; // 로그인된 유저 정보 받기
+  const onDelete = async () => {
+    const ok = confirm("정말 삭제하실 건가요?");
+    if (!ok || user?.uid !== userId) return; // confirm 취소 또는 로그인된 id와 트윗의 id가 같지 않다면 함수 종료(실행x)
+    try {
+      await deleteDoc(doc(db, "hoon", id)); // 매개변수는 삭제할 문서에 대한 참조
+      if (photo) {
+        const photoRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${id}`
+        );
+        // 어떤 사진 삭제할건지 사진 폴더 및 파일 참조
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+        ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
